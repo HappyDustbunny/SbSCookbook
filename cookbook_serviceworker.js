e21731d0b3ac249bcb6b2a8d02838fbcaaa7a941
HappyDustbunny <madshorn@operamail.com>
2023-03-12 16:56:32 +0100

    Update serviceworker.js

--- cookbook_serviceworker.js
+++ cookbook_serviceworker.js
@@ -1,8 +1,10 @@
 
-let VERSION = 1.005; // Change version number to facilitate updates
+let VERSION = 1.006; // Change version number to facilitate updates
 let SBSC_CACHE = 'SBSC-cache';
 let CACHED_URLS = [ // Copy project path from EVERY entry to avoid mistakes
-  'favicons/favicon-192.png', // Check that ALL files have an extension
+  'components/modal-init.js', // Check that ALL files have an extension
+  'components/modal-window.js',
+  'favicons/favicon-192.png',
   'favicons/maskable_192.png',
   'favicons/favicon-512.png',
   'aboutStepByStep.html',
@@ -39,7 +41,7 @@ self.addEventListener('fetch', function(event) {
         return fetch(event.request);
       }
     }).catch(function(error) {
-      console.log('TFT cookbook serviceWorker responded with error', error);
+      console.log('TFT Cookbook serviceWorker responded with error', error);
     })
   );
 })
